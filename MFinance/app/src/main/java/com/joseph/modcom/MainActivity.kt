9aac5dbaf54ae67cb860a70854770eea9755fc31
modcomlearnig <modcomlearnig@gmail.com>
2020-07-09 13:41:17 +0300

    first commit

---
+++ MFinance/app/src/main/java/com/joseph/modcom/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.joseph.modcom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+        //Please change the layout to activity_main or register to run other layouts
+        //Please check Layouts on res/layout
+    }
+}
