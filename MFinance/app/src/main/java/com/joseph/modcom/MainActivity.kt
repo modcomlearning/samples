011fb7f3e86b513145f04f7e89049ea72d4520f9
modcomlearnig <modcomlearnig@gmail.com>
2020-07-16 17:51:28 +0300

    Fifth  Commit

--- MFinance/app/src/main/java/com/joseph/modcom/MainActivity.kt
+++ MFinance/app/src/main/java/com/joseph/modcom/MainActivity.kt
@@ -13,25 +13,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sample)
-        //how do we  connect to our 3 EditTexts, 1 Button, 1 TextView
+        //How do we  connect to our 3 EditTexts, 1 Button, 1 TextView
         //You need to remember the ids you used in XML
-        //make the button listen to click, currently the button is not working
+        //Make the button listen to click, currently the button is not working
         //What is the button id in XML?
-        //am reapeting
+        //Am repeating
+
         submit.setOnClickListener(View.OnClickListener {
-            //what are the ids for our edit text?
+            //What are the ids for our edittext
             val name  = editTextTextPersonName.text   //get text and store in val 'name'
             val pass = editTextTextPassword.text   //get text and store in val 'pass'
             val email = editTextTextEmailAddress.text   //get text and store in val 'email'
 
-            //now show the 3 val above to textview
-            //remove/clear default text
+            //Now show the 3 val above to textview
+            //Remove/clear default text
             tvoutput.setText("")
             tvoutput.append(name.append("\n"))  //append name to Textview
             tvoutput.append(pass.append("\n"))  //append pass to textview
             tvoutput.append(email.append("\n"))  //append email to textview
             //Done
-
         });
     }
 }
