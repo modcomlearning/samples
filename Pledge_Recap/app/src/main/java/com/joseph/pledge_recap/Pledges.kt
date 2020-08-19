90dd7a7f7b225145de8d59d6ed8423c1359ae2e8
modcomlearnig <modcomlearnig@gmail.com>
2020-08-19 15:09:54 +0300

    Fourth Commit

--- Pledge_Recap/app/src/main/java/com/joseph/pledge_recap/Pledges.kt
+++ Pledge_Recap/app/src/main/java/com/joseph/pledge_recap/Pledges.kt
@@ -2,10 +2,22 @@ package com.joseph.pledge_recap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class Pledges : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pledges)
+
+        //get posted pledges from firebase
+        FirebaseApp.initializeApp(this) //initialize your app
+        val rootref: DatabaseReference = FirebaseDatabase.getInstance().reference //your db
+        val demoref: DatabaseReference = rootref.child("members") //table
+
+        //fetching the pledges
+
+
     }
 }
