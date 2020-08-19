63445c850cb7cd20421f4c23e9c907d308a54241
modcomlearnig <modcomlearnig@gmail.com>
2020-08-19 15:28:48 +0300

    Fifth Commit

--- Pledge_Recap/app/src/main/java/com/joseph/pledge_recap/Pledges.kt
+++ Pledge_Recap/app/src/main/java/com/joseph/pledge_recap/Pledges.kt
@@ -3,8 +3,8 @@ package com.joseph.pledge_recap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.FirebaseApp
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_pledges.*
 
 class Pledges : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +17,33 @@ class Pledges : AppCompatActivity() {
         val demoref: DatabaseReference = rootref.child("members") //table
 
         //fetching the pledges
+        val listener = object: ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+                   tvpledges.setText("Database Error!")
+
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //snapshot has the data
+                if (snapshot.exists()){
+                    //okay, snapshot has the colmns,
+                    for (singleSnapshot in snapshot.children){
+                        tvpledges.append("Confirmed : ${singleSnapshot.child("Name")} has pledged. ")
+                        tvpledges.append("KES : ${singleSnapshot.child("Amount")}.")
+                        tvpledges.append("\n\n")
+                    }//end for
+                }
+
+                else {
+                      tvpledges.setText("Error, Not Data")
+                }
+
+            }//end snapshot
+
+        }//end of listener
+        demoref.addValueEventListener(listener) //end
+
+
 
 
     }
